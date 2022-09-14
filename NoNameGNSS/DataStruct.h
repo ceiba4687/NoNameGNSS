9a3a9c8d1a95729b68a7ab7854341398140ef7d7
ceiba_Y <ceiba4687@outlook.com>
2022-09-14 19:39:30 +0800

    初设

---
+++ NoNameGNSS/DataStruct.h
@@ -0,0 +1,32 @@
+#pragma once
+//data and struct used in the GNSS
+const double GM = 3.986005e14;
+
+struct RINEXNAV
+
+{
+	int PRN;
+	double a0, a1, a2;//time gaizheng
+	double IODE, Crs, Deltan, M0;// ORBIT - 1
+	double Cuc, e, Cus, SqrtA;// ORBIT - 2
+	double Toe, Cic, OMEGA, Cis;// ORBIT - 3
+	double i0, Crc, omega, OMEGAdot;// ORBIT - 4
+	double IDOT, GpsWeekNumber, L2C, L2P;// ORBIT - 5
+	double SatAccuracy, SatHealth, TGD, IODC;// ORBIT - 6
+};
+
+struct GPSTime
+{
+	int weekno;
+	double weekSecond;
+};
+
+struct NTime
+{
+	int Year;
+	int Month;
+	int Day;
+	int Hour;
+	int Minute;
+	double Second;
+};
--- NoNameGNSS/main.cpp
+++ NoNameGNSS/main.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
-//using namespace std;
+using namespace std;
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
+	cout << "Hello GNSS!" << endl;
+	//1 read RINEX3
+	//2 cal sate pos
+	//3 spp from rinex
 	return 0;
 }
